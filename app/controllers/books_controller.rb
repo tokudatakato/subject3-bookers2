909447a0c70f7a3f28278be4c895f394e855e85d
TakatoTokuda <119557028+tokudatakato@users.noreply.github.com>
2022-12-16 08:47:24 +0000

    [finish]15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,9 +16,27 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    #redirect_to
   end
   
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :books, dependent: :destroy
+  
+  has_one_attached :image
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/image/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 end
