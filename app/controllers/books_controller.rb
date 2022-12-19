cf1a4f7108caf2ed9c964a7a9c157ff4a23daf0e
TakatoTokuda <119557028+tokudatakato@users.noreply.github.com>
2022-12-19 07:22:24 +0000

    [finish]19

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    #redirect_to
+    redirect_to books_path
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,23 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(user_params[:id])
+    @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
-    @user = User.find(user_params[:id])
+    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(user_params[:id])
+    @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(user_params[:id])
+    redirect_to user_path(params[:id])
   end
 
   def index
     @users = User.all
-    @user = User.find(user_params[:id])
+    @user = User.find(params[:id])
   end
   
   private
